5f645005fe25b7ba46c1a57c326e5dfa9902986b
maker2413 <maker2413@gmail.com>
2025-11-22 23:33:45 -0600

    began embedding text for semantic search

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,11 +2,26 @@
 
 import argparse
 
+from lib.semantic_search import embed_text, verify_model
+
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    subparsers.add_parser("verify", help="Verify that the embedding model is loaded")
+
+    single_embed_parser = subparsers.add_parser(
+        "embed_text", help="Generate an embedding for a single text"
+    )
+    single_embed_parser.add_argument("text", type=str, help="Text to embed")
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
